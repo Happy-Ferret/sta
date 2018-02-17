199277acbbf09bb35c872d0ed84321a1fde897ae
Quentin RIBAC <ribac.quentin@gmail.com>
2018-02-17 23:46:46 +0100

    42

--- main/sta.go
+++ main/sta.go
@@ -2,32 +2,50 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
-	"fmt"
 )
+
+type Command struct {
+	Cmd  string
+	Args []string
+}
+
+func prompt(promptStr string, r *bufio.Reader, w *bufio.Writer) (cmd *Command, err error) {
+	fmt.Fprintf(w, promptStr)
+	err = w.Flush()
+	if err != nil {
+		return nil, err
+	}
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	line = strings.Trim(line, " \n")
+	sections := strings.Split(line, " ")
+	cmd = &Command{sections[0], sections[1:]}
+	return cmd, nil
+}
 
 func main() {
 	// setting I/O
 	r := bufio.NewReader(os.Stdin)
 	w := bufio.NewWriter(os.Stdout)
 
-	fmt.Fprintf(w, "Bienvenue au Royaume de Reosal!\n")
+	fmt.Fprintf(w, "Hello world!\n")
 	w.Flush()
 	for {
-		fmt.Fprintf(w, "> ")
-		w.Flush()
-		line, _ := r.ReadString('\n')
-		line = strings.Trim(line, " \n")
-		args := strings.Split(line, " ")
+		cmd, err := prompt("> ", r, w)
+		if err != nil {
+			break
+		}
 
-		if strings.Compare(args[0], "quit") == 0 {
+		if strings.Compare(cmd.Cmd, "quit") == 0 {
 			break
 		} else {
-			fmt.Fprintf(w, "%v %v\n", args[0], args[1:])
+			fmt.Fprintf(w, "%v %v\n", cmd.Cmd, cmd.Args)
 		}
 		w.Flush()
 	}
-
 }
-
